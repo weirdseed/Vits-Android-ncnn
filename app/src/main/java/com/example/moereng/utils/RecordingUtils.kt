349a9c97df75804be35d2099f07fdfd617e5766a
weirdseed <864312916@qq.com>
2023-01-20 23:38:43 +0800

    fix

--- app/src/main/java/com/example/moereng/utils/RecordingUtils.kt
+++ app/src/main/java/com/example/moereng/utils/RecordingUtils.kt
@@ -48,7 +48,7 @@ class RecordingUtils {
         // begin recording
         recorder?.startRecording()
         recorder?.read(audio!!, 0, minBufferSize, AudioRecord.READ_BLOCKING)
-        return audio
+        return audio?.map { it * 10 }?.toFloatArray()
     }
 
     fun stop(){
